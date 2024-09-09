ad39675906e52b0bc6340d2176e7e4689a71a620
Stefan Alfbo <stefan@alfbo.se>
2024-09-09 21:12:50 +0200

    Create a unit test to verify that the enterRaffle function emits the correct event
    The event is refactored to an interface to make it available in unit tests.

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -4,23 +4,30 @@ pragma solidity 0.8.19;
 // Custom errors
 error Raffle__SendMoreToEnterRaffle();
 
+interface RaffleEvent {
+    event RaffleEntered(address indexed player);
+}
+
 /**
  * @title A sample Raffle contract
  * @author Stefan Alfbo
  * @notice This contract is for creating a sample raffle
  * @dev Implements Chainlink VRFv2.5
  */
-contract Raffle {
+contract Raffle is RaffleEvent {
     uint256 private immutable i_entranceFee;
+    // @dev The duration of the raffle in seconds
+    uint256 private immutable i_interval;
     address payable[] private s_players;
+    uint256 private s_lastTimeStamp;
 
-    event RaffleEntered(address indexed player);
-
-    constructor(uint256 entranceFee) {
+    constructor(uint256 entranceFee, uint256 interval) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
     }
 
-    function enterRaffle() public payable {
+    function enterRaffle() external payable {
         if (msg.value < i_entranceFee) {
             revert Raffle__SendMoreToEnterRaffle();
         }
@@ -30,7 +37,9 @@ contract Raffle {
     }
 
     function pickWinner() public {
-        // Pick the winner
+        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
+            revert();
+        }
     }
 
     /** Getter functions **/
--- test/Raffle.t.sol
+++ test/Raffle.t.sol
@@ -2,17 +2,30 @@
 pragma solidity 0.8.19;
 
 import {Test} from "forge-std/Test.sol";
-import {Raffle, Raffle__SendMoreToEnterRaffle} from "../src/Raffle.sol";
+import {Raffle, Raffle__SendMoreToEnterRaffle, RaffleEvent} from "../src/Raffle.sol";
 
-contract RaffleTest is Test {
+contract RaffleTest is Test, RaffleEvent {
     Raffle raffle;
+    address USER = makeAddr("user");
+    uint256 constant ENTRANCE_FEE = 100;
+    uint256 constant INTERVAL = 60;
 
     function setUp() public {
-        raffle = new Raffle(100);
+        raffle = new Raffle(ENTRANCE_FEE, INTERVAL);
     }
 
     function testEnterRaffleWithToLittleEth() public {
         vm.expectRevert(Raffle__SendMoreToEnterRaffle.selector);
         raffle.enterRaffle{value: 42}();
+    }
+
+    function testEnterRaffle() public {
+        vm.deal(USER, ENTRANCE_FEE);
+        vm.prank(USER);
+        vm.expectEmit();
+        emit RaffleEntered(USER);
+
+        raffle.enterRaffle{value: ENTRANCE_FEE}();
+        assert(raffle.getEntranceFee() == ENTRANCE_FEE);
     }
 }
